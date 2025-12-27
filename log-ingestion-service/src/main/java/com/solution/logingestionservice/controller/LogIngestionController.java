e3c4bcc14479956139d152776bd389451ea8a438
Burakevich Vladislav <burakevichvalli040@gmail.com>
2025-12-27 09:22:57 +0300

    feature: init minimal log-ingestion-service

---
+++ log-ingestion-service/src/main/java/com/solution/logingestionservice/controller/LogIngestionController.java
@@ -0,0 +1,27 @@
+package com.solution.logingestionservice.controller;
+
+
+import com.solution.logingestionservice.dto.LogEventDto;
+import com.solution.logingestionservice.service.LogIngestionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/logs")
+@RequiredArgsConstructor
+public class LogIngestionController {
+    private final LogIngestionService ingestionService;
+
+    @PostMapping("/batch")
+    public ResponseEntity<Void> ingestLog(@Valid @RequestBody List<LogEventDto> logEvent) {
+        ingestionService.sendBatchToKafka(logEvent);
+        return ResponseEntity.accepted().build();
+    }
+}
---
+++ log-ingestion-service/src/main/java/com/solution/logingestionservice/service/LogIngestionService.java
@@ -0,0 +1,55 @@
+package com.solution.logingestionservice.service;
+
+
+import com.solution.logingestionservice.dto.LogEventDto;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+public class LogIngestionService {
+    private final KafkaTemplate<String, LogEventDto> kafkaTemplate;
+    private final String topic;
+
+    //TODO добавить логику проверки accountId
+
+    public LogIngestionService(
+            KafkaTemplate<String, LogEventDto> kafkaTemplate,
+            @Value("${KAFKA_TOPIC_LOGS_RAW:logs.raw}") String topic
+    ) {
+        this.kafkaTemplate = kafkaTemplate;
+        this.topic = topic;
+    }
+
+    public void sendToKafka(LogEventDto logEvent) {
+        String key = logEvent.getAccountId();
+
+        kafkaTemplate.send(topic, key, logEvent)
+            .whenComplete(
+                (result, ex) -> {
+                    if (ex == null) {
+                        log.info("Log sent successfully: {}", logEvent);
+                    } else {
+                        log.error("Log sent failed: {}", ex.getMessage());
+                    }
+                }
+            );
+    }
+
+    public void sendBatchToKafka(List<LogEventDto> batch) {
+        for (LogEventDto logEvent : batch) {
+            String key = logEvent.getAccountId();
+
+            kafkaTemplate.send(topic, key, logEvent)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log.error("Failed to send log for account: {}", key);
+                    }
+                });
+        }
+    }
+}
